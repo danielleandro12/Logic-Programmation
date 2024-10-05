779ae029b2ba2793649b1d6b6c7e3b94339b12d4
Daniel Leandro <166567188+danielleandro12@users.noreply.github.com>
2024-10-05 10:25:47 -0300

    Add files via upload

---
+++ Aula01/exercicios.py
@@ -0,0 +1,72 @@
+# Exercícios - Lógica de programação em Python
+
+# Fundamentos:
+
+# 1  - Crie um algoritmo que exiba na tela (terminal) a mensagem "Olá Mundo".
+print('Hello World')
+
+# 2  - Crie um algoritmo que exiba na tela (terminal) seu nome. Por exemplo: "Olá, João Paulo."
+saudaçao = 'Olá, Daniel Leandro.'
+print(saudaçao)
+nome = 'Daniel'
+# 3  - Crie um algoritmo que exiba na tela (terminal) sua idade. Por exemplo: 17.
+idade = 21
+print(idade)
+
+# 4  - Crie um algoritmo que exiba na tela (terminal) (em linhas separadas) seu nome e sua idade.
+print(nome)
+print(idade)
+# 5  - Crie um algoritmo que exiba na tela (terminal) (na mesma linha) seu nome e sua idade.
+print('Daniel,vinte um anos')
+
+# 6  - Crie um algoritmo que exiba na tela (terminal) a soma de dois números inteiros.
+print(9 + 18)
+
+# 7  - Crie um algoritmo que exiba na tela (terminal) a subtração de dois números inteiros.
+print(74 - 56)
+
+# 8  - Crie um algoritmo que exiba na tela (terminal) a multiplicação de dois números inteiros.
+print(60 * 8)
+
+# 9  - Crie um algoritmo que exiba na tela (terminal) a divisão de dois números inteiros.
+print(77 / 7)
+
+# 10 - Crie um algoritmo que exiba na tela (terminal) o resto da divisão de dois números inteiros.
+print(30 % 7)
+
+# 11 - Crie um algoritmo que exiba na tela (terminal) apenas a parte inteira de uma divisão de dois números inteiros.
+print(92 // 6)
+
+# 12 - Crie um algoritmo que exiba na tela (terminal) o quadrado de um número inteiro.
+print(5 ** 3)
+
+# 13 - Crie um algoritmo que exiba na tela (terminal) o valor lógico de uma variável qualquer.
+print(bool(30))
+
+# 14 - Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é "jpaulo@infinityschoool.com.br".
+print(type('jpaulo@infinityschool.com.br'))
+
+# 15 - Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é "c".
+print(type('c'))
+
+# 16 - Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é 12.
+print(type(12))
+
+# 17 - Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é 1.87.
+print(type(1.87))
+# 18 - Crie um algoritmo que exiba na tela (terminal) o tipo de uma variável cujo valor atribuído é False.
+print(type(False))
+
+# 19 - Crie um algoritmo que concatene duas strings ao ser executado. Exiba a string concatenada na tela (terminal).
+copo = 'suco'
+prato = 'sobremesa'
+print(copo + prato)
+
+# 20 - Crie um algoritmo que concatene dois numeros inteiros ao ser executado. Exiba os números concatenados na tela (terminal).
+altura = 1.59
+print(idade + altura)
+
+
+#funçao interna input
+nome = input("Informe seu nome")
+print(f"Olá {nome} , É um prazer te ver por aqui")
---
+++ Aula01/variaveis.py
@@ -0,0 +1,106 @@
+# prato = 'comida' 
+#variavel do tipo string
+
+#altura = 1.81 
+#variavel do tipo float(ponto flutuante) ,numero real;
+
+#Variavel do tipo inteiro;
+#idade = 21 
+
+#Variavel do tipo booleano;
+#brasileiro = True 
+
+#print('Hello Word')
+#print(brasileiro)
+#rint(idade)
+#print(prato)
+nome = 'Daniel'
+
+altura = 1.80
+
+mes = 10
+
+Estudante = True
+
+print(nome)
+print(altura)
+print(mes)
+print(Estudante)
+
+#Funçao interna type
+print(type('Daniel'))
+print(type(10))
+print(type(1.80))
+print(type(Estudante))
+
+#Operadores de comparaçao
+print(5 == 5) #True
+
+print(7 < 4) #False
+
+print(9 > 12) #False
+
+print(10 != 10) #False
+
+print(9 >= 9) #True
+
+print(6 <= 7) #True
+
+#Operaçoes aritmeticas
+print(3 + 7) #10
+
+print(90 / 9) #10
+
+print(8 * 7) #56
+
+print(25 - 12) #13
+
+print(23 // 7) #3
+
+print(3 ** 3) #27
+
+print(25 % 3) #1
+
+# Arquivo da aula 1
+
+# Declaração de Variáveis
+
+copo = 'água'  #essa é uma variável do tipo string
+
+altura = 1.82  #essa é uma variável do tipo float (ponto flutuante), numero real;
+
+idade = 22  #Essa é uma variável do tipo inteiro;
+
+brasileiro = True #Essa é uma variável do tipo booleano;
+
+
+#Operadores de comparação
+print(4 == 4) # True
+
+print(7 < 3) # False
+
+print(3 <= 5) # True
+
+print(4 != 5) #True
+
+
+#Operações Aritimeticas
+print(altura + idade) # Soma
+
+print(idade - 42) # Subtração
+
+print(altura * 2)  # Multiplicação
+
+print(31 / 5) # 31.2 (Divisão comum)
+
+print(3 ** 2) # 9  (Potenciação)
+
+print(25 // 6) # 4 (Divisão inteira)
+
+print(25 % 2) # 1  (Módulo de, ou resto da divisão inteira)
+
+
+
+
+
+
