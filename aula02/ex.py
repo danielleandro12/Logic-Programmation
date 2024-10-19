74983731a884d9994d276a7544875c11359f695d
Daniel Leandro <166567188+danielleandro12@users.noreply.github.com>
2024-10-19 10:29:44 -0300

    Add files via upload

---
+++ aula02/ex.py
@@ -0,0 +1,70 @@
+
+#Exercícios:
+
+#Exercícios - Lógica de programação em Python
+
+# Input - para os exercícios abaixo você precisará utilizar o método input() para requisitar dados externos ao programa:
+
+# 1  - Crie um algoritmo em que o usuário informe uma frase qualquer e exiba a frase na tela (terminal) quando executado.
+frase = input("Digite uma frase :")
+# 2  - Crie um algoritmo em que o usuário informe o nome completo e exiba o nome completo na tela (terminal) quando executado.
+nome = input("Digite seu nome completo:")
+# 3  - Crie um algoritmo em que o usuário informe a idade e exiba a idade na tela (terminal) quando executado.
+idade = input("Digite sua idade :")
+# 4  - Crie um algoritmo em que o usuário informe dois números inteiros (n1 e n2) e exiba os números na tela (terminal) quando executado.
+numero1 =int(input("Digite um numero"))
+
+numero2 =int(input("Digite outro numero"))
+
+# 5  - Crie um algoritmo em que o usuário informe dois números inteiros (n1 e n2) e exiba a soma dos números na tela (terminal) quando executado.
+numero1 =int(input("Digite um numero"))
+
+numero2 =int(input("Digite outro numero"))
+
+print(numero1 + numero2)
+# 6  - Crie um algoritmo em que o usuário informe dois números inteiros (x1 e x2) e exiba a diferença dos números na tela (terminal) quando executado.
+numero1 =int(input("Digite um numero"))
+
+numero2 =int(input("Digite outro numero"))
+
+print(numero1 - numero2)
+# 7  - Crie um algoritmo em que o usuário informe dois números inteiros (a1 e a2) e exiba o produto dos números na tela (terminal) quando executado.
+numero1 =int(input("Digite um numero"))
+
+numero2 =int(input("Digite outro numero"))
+
+print(numero1 * numero2)
+# 8  - Crie um algoritmo em que o usuário informe dois números inteiros (x e y) e exiba o quociente dos números na tela (terminal) quando executado.
+numero1 =int(input("Digite um numero"))
+
+numero2 =int(input("Digite outro numero"))
+
+print(numero1 / numero2)
+# 9  - Crie um algoritmo em que o usuário informe quatro notas (n1, n2, n3 e n4). Após, calcule a média aritmética e exiba a média na tela (terminal) quando executado.
+nota1 = int(input("Digite uma nota"))
+
+nota2 = int(input("Digite uma nota"))
+
+nota3 = int(input("Digite uma nota"))
+
+nota4 = int(input("Digite uma nota"))
+
+#print(nota1 + nota2 + nota3 + nota4 /4)
+#Desafio: Crie um algorimo que peça ao usuário o email e a senha. Caso o usuário e a senha estejam corretos imprima no terminal:
+#"Acesso garantido", caso contrário imprima "Acesso negado"; Pre-defina a senha e e-mail corretos e compare com as entradas obtidas através do input.
+usuario = input("Digite seu usuario :")
+
+senha= input("Digite sua senha :")
+
+if usuario and senha
+
+
+
+#Desafio: Crie um algoritmo que rece
+# o nome, idade, a fruta favorita e se o usuário é ou não aluno da Infinity School.
+# Imprima os valores no seguindo o exemplo:
+# Se as informações forem: "João", 34, "banana", as informações devem aparecer dessa forma no terminal:
+# Nome: João
+# Idade: 34
+# Fruta favorita: banana
+# Aluno Infinty: Sim  
---
+++ aula02/input.py
@@ -0,0 +1,46 @@
+idade1 = int(input("Digite sua idade"))
+
+idade2= int(input("Digite sua idade"))
+
+if idade1 > idade2:
+    print(" idade1 é maior que a idade2") 
+elif idade1 < idade2:
+    print("idade1 é menor que a idade2")
+else  :
+    print("idade1 é igual a idade2")
+
+palavra1 = input("Digite uma palavra")
+     
+palavra2 = input("Digite outra palavra")
+
+if palavra1 != palavra2 :
+    print("as  palavras sao diferentes")
+else :
+    print("as palavras são iguais")    
+idade = int(input("Digite sua idade"))
+
+habilitacao = input("Voce possui habilitacao")
+
+if idade >= 18:
+    print("Voce pode fazer aulas para dirigir")
+elif idade < 18:
+    print("Voce não pode fazer aulas para dirigir")   
+
+
+nota1 = int(input("Digite uma nota"))
+
+nota2 = int(input("Digite outra nota"))
+
+if nota1 >= 6 :
+    print("Essa nota é igual ou maior que 6")
+elif nota2 >= 6 :
+    print(f"{nota2} é igual ou maior que 6") 
+
+
+
+
+
+
+
+
+
