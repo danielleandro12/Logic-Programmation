6546c768cb5fc5ba5812442817d540213b12f68b
Daniel Leandro <166567188+danielleandro12@users.noreply.github.com>
2024-10-26 10:25:27 -0300

    Update exercises.py

--- aula03/exercises.py
+++ aula03/exercises.py
@@ -3,38 +3,38 @@
 # Condicionais:
 
 # 01 - Crie um algoritmo em que o usuário informe dois números (n1 e n2). Após, exiba na tela (terminal) qual deles é o maior.
-#numero1 =int(input("Digite um número"))
+numero1 =int(input("Digite um número"))
 
-#numero2 =int(input("Digite outro número"))
+numero2 =int(input("Digite outro número"))
 
-#if numero1 > numero2:
-#    print("O numero 1 e maior")
-#else:
-#    print("O numero 2 e maior ")
+if numero1 > numero2:
+    print("O numero 1 e maior")
+else:
+    print("O numero 2 e maior ")
 # 02 - Crie um algoritmo em que o usuário informe dois números (n1 e n2). Após, exiba na tela (terminal) qual deles é o menor.
-# numero1 =int(input("Digite um número"))
+ numero1 =int(input("Digite um número"))
 
-# numero2 =int(input("Digite outro número"))
+ numero2 =int(input("Digite outro número"))
 
-# if numero1 < numero2 :
-#     print("O numero 1 e menor")
-# else :
-#     print("O numero 2 e menor")    
+ if numero1 < numero2 :
+     print("O numero 1 e menor")
+ else :
+     print("O numero 2 e menor")    
 # 08 - Crie um algoritmo em que o usuário informe seu gênero, sua altura e seu peso. Após, calcule e exiba na tela (terminal) o IMC do usuário.
-#genero = input("Informe seu genero:")
-#altura =int(input("Digite sua altura"))
-#peso = int(input("Digite seu peso"))
-
-#imc = int(input(altura + peso**2))
+genero = input("Informe seu genero:")
+altura =int(input("Digite sua altura"))
+peso = int(input("Digite seu peso"))
+imc = int(input(altura + peso**2))
 
 # 08 - Crie um algoritmo em que o usuário informe um número qualquer. Após, informe se esse número é par ou ímpar.
-#numero =int(input("Digite um numero"))
-#if numero %2 == 0 :
-#   print("O numero e par")
-#else:
-#   print("O numero e impar")
+numero =int(input("Digite um numero"))
+if numero %2 == 0 :
+   print("O numero e par")
+else:
+print("O numero e impar")
 
 # 10 - Crie um algoritmo em que o usuário informe um número qualquer. Após, informe se esse número é múltiplo de 2.
+#Refazer esse exercício pois o código não está funcionando!!!
 numero =int(input("Digite um numero"))
 if numero %2 ==2 :
    print ("é multiplo")
