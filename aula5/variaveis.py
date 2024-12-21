07e1779bdec286c875d799c05e19259a5d9b6b1d
Daniel Leandro <166567188+danielleandro12@users.noreply.github.com>
2024-12-21 09:11:48 -0300

    Update variaveis.py

--- aula5/variaveis.py
+++ aula5/variaveis.py
@@ -1,14 +1,14 @@
 ##Exercício 1
 #Crie uma variável chamada nome para armazenar o seu nome e outra chamada idade para armazenar sua idade. Depois, imprima as duas variáveis em uma frase.
-#nome = 'Daniel'
-#idade = 21
-
+nome = 'Daniel'
+idade = 21
+print(nome + idade )
 ##Exercício 2
 #Declare três variáveis: a, b e c, atribua valores a elas e calcule a soma. Exiba o resultado.
-#numero1 = 10
-#numero2 = 5
-#numero3 = 1
-#print(numero1+numero2+numero3)
+numero1 = 10
+numero2 = 5
+numero3 = 1
+print(numero1+numero2+numero3)
 
 ##Exercício 3
 #Armazene o valor de uma temperatura em graus Celsius em uma variável e, em seguida, converta para Fahrenheit usando a fórmula:
@@ -23,14 +23,14 @@ print(salario * 0.10)
 
 ##Exercício 6
 #Crie uma variável chamada valor_inicial com um número qualquer. Subtraia 7, multiplique por 2, e depois adicione 10 ao resultado. Exiba o valor final.
-#valor_inicial = 20
-#print(valor_inicial - 7 * 2 + 10)
+valor_inicial = 20
+print(valor_inicial - 7 * 2 + 10)
 
 ##Exercício 7
 #Peça ao usuário para digitar o nome dele e a idade (usando a função input). Armazene esses valores em variáveis e mostre uma mensagem de boas-vindas.
-#nome = input("Digite seu nome:")
-#idade = int(input("Digite sua idade:"))
-#print(f"Olá,{nome}! É muito bom te ver aqui. Seja bem vindo(a)!")
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+print(f"Olá,{nome}! É muito bom te ver aqui. Seja bem vindo(a)!")
 #Dica :Use a f-string para formatar a mensagem, inserindo o valor da variável nome diretamente na frase.
 
 ##Exercício 8
